72c6823c9ff2e1f7ab5661778de55ea24c6925c8
helkarakse <nimcuron@gmail.com>
2013-12-05 18:13:56 +0800

    Added placeholder main file and startup file.

---
+++ src/main.lua
@@ -0,0 +1,16 @@
+--[[
+
+	RSS Display Board Version 1.0 Dev
+	Do not modify, copy or distribute without permission of author
+	Helkarakse, 20131205
+
+]]
+
+os.loadAPI("functions")
+os.loadAPI("xml")
+
+local function init()
+
+end
+
+init()
