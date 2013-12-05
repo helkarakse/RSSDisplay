924538ce55afa56dd6770f866604a5940551becf
helkarakse <nimcuron@gmail.com>
2013-12-05 18:24:18 +0800

    Added code to download the xml file into a string and feed it into the parser.

--- src/main.lua
+++ src/main.lua
@@ -6,11 +6,35 @@
 
 ]]
 
+-- Libs
 os.loadAPI("functions")
 os.loadAPI("xml")
 
-local function init()
+-- Variables
+local parser = xml.XL:new()
+local remoteFile = "http://www.otegamers.com/index.php?app=core&module=global&section=rss&type=forums&id=24"
+local xmlString
 
+-- Functions
+local function getXML()
+	local data = http.get(remoteFile)
+	if (data) then
+    	functions.debug("XML file successfully retrieved.")
+		xmlString = data.readAll()
+		return true
+	else
+		functions.debug("Could not retrieve xml file.")
+		return false
+	end
+end
+
+local function init()
+	local xmlSuccess = getXML()
+	if (xmlSuccess) then
+		parser:from_string(xmlString)
+		local channel = parser:find(parser:root(), "channel")
+		print(channel)
+	end
 end
 
 init()
