ac099de8de8d0691383ddc927774fa31ba298747
helkarakse <nimcuron@gmail.com>
2013-12-05 19:17:52 +0800

    Added porcelain functions for title, desc, link and pub date removal. Also added items retrieval.

--- src/parser.lua
+++ src/parser.lua
@@ -19,3 +19,39 @@ function parseData(xmlString)
 	parser:from_string(xmlString)
 	tagChannel = parser:find(parser:root(), "channel")
 end
+
+-- Returns the title of the feed
+function getTitle()
+	return tagChannel[1][1][1]
+end
+
+-- Returns the description of the feed
+function getDescription()
+	return tagChannel[1][2][1]
+end
+
+-- Returns the link component of the feed
+function getLink()
+	return tagChannel[1][3][1]
+end
+
+-- Returns the published date of the feed
+function getPubDate()
+	return tagChannel[1][4][1]
+end
+
+function getItems(id)
+	local specificId = tonumber(id) or 0
+	if (specificId == 0) then
+		-- no specific id passed, assume return all
+		local array = {}
+		for i = tonumber(id) + 5, #tagChannel[1] do
+			table.insert(array, tagChannel[1][i])
+		end
+		
+		return array
+	else
+		-- return specific item
+		return tagChannel[1][id + 5]
+	end
+end
